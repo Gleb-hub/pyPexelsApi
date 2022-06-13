13a5cc0cc35996acd2cded596baf0165da1abe7d
gl_epka <nechaevgleb@inbox.ru>
2022-06-13 14:11:34 +0300

    Small fixes

--- api.py
+++ api.py
@@ -34,51 +34,55 @@ class API:
     def __init__(self, API_KEY: str):
         self.PEXELS_AUTHORIZATION = {"Authorization": API_KEY}
         self.request = None
+        self.parsed = None
 
     '''Public photos methods'''
     def search_photos(self, params: search_params) -> Photo_request:
         url = 'https://api.pexels.com/v1/search'
         self.__request(url, params)
-        return Photo_request(self.request)
+        return Photo_request(self.parsed)
 
     def get_curated(self, params: get_curated_params) -> Photo_request:
         url = 'https://api.pexels.com/v1/curated'
         self.__request(url, params)
-        return Photo_request(self.request)
+        return Photo_request(self.parsed)
 
     def get_photo(self, id: Union[int, str]) -> Photo:
         url = 'https://api.pexels.com/v1/photos/' + str(id)
         self.__request(url, None)
-        return Photo(self.request)
+        return Photo(self.parsed)
 
     '''Public video methods'''
     def search_videos(self, params: search_params) -> Video_request:
         url = 'https://api.pexels.com/videos/search'
         self.__request(url, params)
-        return Video_request(self.request)
+        return Video_request(self.parsed)
 
-    def get_popular(self, params: get_popular_params) -> Video_request:
+    def get_popular(self, params: Union[get_popular_params, None]) -> Video_request:
         url = 'https://api.pexels.com/videos/popular'
         self.__request(url, params)
-        return Video_request(self.request)
+        return Video_request(self.parsed)
 
     def get_video(self, id: Union[int, str]) -> Video:
         url = 'https://api.pexels.com/v1/videos/' + str(id)
         self.__request(url, None)
-        return Video(self.request)
+        return Video(self.parsed)
 
     '''Private methods'''
     def __request(self, url: str, params: Union[dict, None]):
         try:
             self.request = requests.get(url, params=params, timeout=15, headers=self.PEXELS_AUTHORIZATION)
-
+            self.parsed = self.request.json()
             if not self.request.ok:
                 print('Wrong response you might have a wrong API key')
                 print(self.request)
                 print(f'API key: {self.PEXELS_AUTHORIZATION}')
                 self.request = None
+                self.parsed = None
                 exit()
+
         except requests.exceptions.RequestException:
             print('Request failed, check your internet connection')
             self.request = None
+            self.parsed = None
             exit()
--- tools/photographer.py
+++ tools/photographer.py
@@ -3,3 +3,6 @@ class Photographer:
         self.photographer: str = ph
         self.photographer_url: str = url
         self.photographer_id: int = id
+
+    def __str__(self):
+        return f'name: {self.photographer}\nurl: {self.photographer_url}\nid: {self.photographer_id}'
