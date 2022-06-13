2701905e20fecc97fb69014b03af9697168ddc58
gl_epka <nechaevgleb@inbox.ru>
2022-06-13 03:39:49 +0300

    Python Pexels Api Alpha

---
+++ tools/photographer.py
@@ -0,0 +1,5 @@
+class Photographer:
+    def __init__(self, ph: str, url: str, id: int):
+        self.photographer: str = ph
+        self.photographer_url: str = url
+        self.photographer_id: int = id
